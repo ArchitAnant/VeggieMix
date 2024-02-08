bab6c75841d1472d0b9cb1c4e792d79b3f0f4854
Archit Anant <architanant5@gmail.com>
2024-02-08 20:29:15 +0530

    Player Page Completion!

---
+++ UI/Startup/PlayerPage.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView 2.swift
+//  
+//
+//  Created by Archit Anant on 08/02/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView_2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView_2()
+}
